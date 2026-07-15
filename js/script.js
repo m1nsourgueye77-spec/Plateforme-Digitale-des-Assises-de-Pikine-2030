91f2eacfd3878a6f2256aa223c6e80a96ad17ab7
Africa Light Site <m1nsour.gueye77@gmail.com>
2026-07-15 22:28:18 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -394,7 +394,7 @@ function chargerThemesDiaspora() {
 // Contribution
 function chargerThemesContribution() {
 
-    const axe = document.getElementById("axe").value;
+    const axe = document.getElementById("axeContribution").value;
     const zone = document.getElementById("themesContribution");
 
     afficherThemes(axe, zone, "themesContribution");
